a10907e1204d147b8ef4d315bc907b2fccd59b74
cpuritz <connorpuritz2025@u.northwestern.edu>
2025-10-23 14:26:24 -0500

    Updated timing code

--- 05times/time.R
+++ 05times/time.R
@@ -8,17 +8,24 @@ family <- commandArgs(trailingOnly = TRUE)[1]
 task <- as.integer(commandArgs(trailingOnly = TRUE)[2])
 cores <- as.integer(commandArgs(trailingOnly = TRUE)[3])
 
-sce_all <- readRDS("Data/References/Time/bailey24-tram1.rds")
+sce_all <- readRDS("Data/References/Subsets/bailey24-tram1.rds")
 
 genes <- c(10, 20, 30, 40, 50)
 cells <- c(100, 1000, 2000, 3000, 4000, 5000)
+
 params <- expand.grid(genes, cells)
 
 ng <- params[task, 1]
 nc <- params[task, 2]
 
 message("Timing for family = ", family, ", genes = ", ng, ", cells = ", nc)
-nrun <- 100L
+
+if (family %in% c("norm", "norm_jitter")) {
+    nrun <- 100L
+} else {
+    nrun <- 3L
+}
+
 times <- parallel::mclapply(seq(nrun), function(i) {
     # Randomly subsample
     cells <- sample(dim(sce_all)[2], nc)
@@ -64,5 +71,5 @@ df <- data.frame(
     cells = rep(nc, nrun),
     time = unlist(times)
 )
-saveRDS(df, file = paste0("Results/04times/", family, "_", ng, "_", nc, ".rds"))
+saveRDS(df, file = paste0("Results/05times/", family, "_", ng, "_", nc, ".rds"))
 message("Done!")
