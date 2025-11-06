916388f39794651a574cabb08d333e97726bfb17
cpuritz <connorpuritz2025@u.northwestern.edu>
2025-11-06 12:02:07 -0600

    Updated shuffle code

--- 01data/build_shuffles.R
+++ 01data/build_shuffles.R
@@ -1,11 +1,18 @@
 library(SingleCellExperiment)
 set.seed(1)
 
-refs <- list.files("Data/References/Subsets")
-ntrial <- 50L
+refs_all <- list.files("Data/References/Subsets")
 
+refs <- refs_all[grepl("hsa|HVG", refs_all)]
 for (ref in refs) {
     sce <- readRDS(paste("Data/References/Subsets", ref, sep = '/'))
-    shuffles <- t(replicate(ntrial, sample(dim(sce)[2])))
+    shuffles <- t(replicate(50L, sample(dim(sce)[2])))
+    saveRDS(shuffles, file = paste("Results/01shuffles", ref, sep = '/'))
+}
+
+refs <- refs_all[grepl("hdwgcna", refs_all)]
+for (ref in refs) {
+    sce <- readRDS(paste("Data/References/Subsets", ref, sep = '/'))
+    shuffles <- t(replicate(10L, sample(dim(sce)[2])))
     saveRDS(shuffles, file = paste("Results/01shuffles", ref, sep = '/'))
 }
