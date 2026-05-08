021b6956ce9381396577dda090ad7bee50e9895a
cpuritz <connorpuritz2025@u.northwestern.edu>
2026-05-08 08:24:37 -0500

    Revision

--- 01shuffles/build_shuffles.R
+++ 01shuffles/build_shuffles.R
@@ -4,15 +4,17 @@ set.seed(1)
 refs_all <- list.files("Data/References/Subsets")
 
 refs <- refs_all[grepl("hsa|HVG", refs_all)]
+ntrial <- 50L
 for (ref in refs) {
     sce <- readRDS(paste("Data/References/Subsets", ref, sep = '/'))
-    shuffles <- t(replicate(50L, sample(dim(sce)[2])))
+    shuffles <- t(replicate(ntrial, sample(dim(sce)[2])))
     saveRDS(shuffles, file = paste("Results/01shuffles", ref, sep = '/'))
 }
 
+ntrial <- 10L
 refs <- refs_all[grepl("hdwgcna", refs_all)]
 for (ref in refs) {
     sce <- readRDS(paste("Data/References/Subsets", ref, sep = '/'))
-    shuffles <- t(replicate(10L, sample(dim(sce)[2])))
+    shuffles <- t(replicate(ntrial, sample(dim(sce)[2])))
     saveRDS(shuffles, file = paste("Results/01shuffles", ref, sep = '/'))
 }
